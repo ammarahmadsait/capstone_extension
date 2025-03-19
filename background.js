8480bb0a16be3fdbce1f0b1e8ee3d1799b254449
ammarahmadsait <ammar.ahmad@edu.sait.ca>
2025-03-19 15:37:39 -0600

    temporarily removing account functionality from service worker

--- background.js
+++ background.js
@@ -1,15 +1,3 @@
-chrome.identity.getAuthToken({ interactive: true }, function (token) {
-    if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-    } else {
-        fetch("https://www.googleapis.com/oauth2/v3/userinfo", {
-            headers: { Authorization: `Bearer ${token}` }
-        })
-            .then(response => response.json())
-            .then(userInfo => {
-                console.log("User Info:", userInfo);
-                chrome.storage.sync.set({ user: userInfo });
-            })
-            .catch(error => console.error("Error fetching user info:", error));
-    }
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Cyber Security Chatbot extension installed.");
 });
